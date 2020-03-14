c3e1a2689ed81c63633535dd27208ff5cd4a2f3a
Can <canerdogar94@gmail.com>
2020-03-14 17:40:01 +0300

    component added to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BusStopList} from "./busStopList/BusStopList";
+import {Container, Row} from "react-bootstrap";
+import {PaymentForm} from "./payment/PaymentForm";
+import {Map} from "./map/Map";
+import {BusStop} from "./entityStore/BusStopService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+
+}
+
+interface AppState {
+  openedPanel: PanelType | null;
+  busStop: BusStop | null;
+}
+
+enum PanelType {
+  PAYMENT,
+  MAP,
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+        openedPanel: null,
+        busStop: null,
+    };
+  }
+
+  private showPaymentForm = (busStop: BusStop) => {
+    this.setState({
+        openedPanel: PanelType.PAYMENT,
+        busStop
+    })
+  }
+
+  private showMap = (busStop: BusStop) => {
+      this.setState({
+          openedPanel: PanelType.MAP,
+          busStop
+      })
+  }
+
+  render() {
+      return (
+          <Container fluid className="d-flex h-100 flex-column">
+              <Row className="flex-fill d-flex justify-content-center align-items-center">
+                  <BusStopList
+                      onMapClick={this.showMap}
+                      onDonationClick={this.showPaymentForm}
+                      transparent={this.state.openedPanel !== null}
+                  />
+                  {this.state.openedPanel === PanelType.PAYMENT &&
+                    <PaymentForm
+                        busStop={this.state.busStop}
+                    />
+                  }
+                  {this.state.openedPanel === PanelType.MAP &&
+                  <Map
+                    busStop={this.state.busStop}
+                  />
+                  }
+              </Row>
+          </Container>
+      );
+  }
+
 }
 
 export default App;
