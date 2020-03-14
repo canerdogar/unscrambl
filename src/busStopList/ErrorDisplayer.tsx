52c58aa5431016f9bebc0d8e76822469e281ab0d
Can <canerdogar94@gmail.com>
2020-03-14 18:12:30 +0300

    retry icon is now centered

--- src/busStopList/ErrorDisplayer.tsx
+++ src/busStopList/ErrorDisplayer.tsx
@@ -21,7 +21,7 @@ export class ErrorDisplayer extends React.Component<ErrorDisplayerProps, ErrorDi
         return (
             <div className="retry d-flex align-items-start flex-column" onClick={this.props.refresh}>
                 <h6>Fetching list of bus stop failed. Click to retry.</h6>
-                <div className="d-flex justify-content-center">
+                <div className="d-flex justify-content-center w-100">
                     <FontAwesomeIcon icon={faRedo} className="iconContainer m-1"/>
                 </div>
             </div>
